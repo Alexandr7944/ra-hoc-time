f592ffc2299d69433073d756ae4e8d88d3970c0e
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2023-06-28 09:24:33 +0300

    initial commit

---
+++ src/components/DateTime.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line react/prop-types
+const DateTime= ({ date }) => {
+
+  return (
+    <p className="date">{ date }</p>
+  )
+}
+
+export default DateTime
---
+++ src/components/Video.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import DateTimePretty from "./DateTimePretty";
+
+export const Video = ({ url, date}) => {
+
+  return (
+    <div className="video">
+      <iframe
+        src={url}
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+      ></iframe>
+      <DateTimePretty date={date} />
+    </div>
+  )
+};
